9ec2674c52ff5f71b25e538cdf87d025d55f1536
Jamie Berrier <berrier.jamie@gmail.com>
2019-11-15 19:39:14 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191115193251) do
+ActiveRecord::Schema.define(version: 20191115193830) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
